8947d65ee2d9cc955fb45394997baa267ed01250
Jake Morrison <tech@techthoughts.info>
2020-09-12 16:59:00 -0700

    s3 deploy adjustments

--- CodeBuild/IntegrationTest/Tests/Integration.Tests.ps1
+++ CodeBuild/IntegrationTest/Tests/Integration.Tests.ps1
@@ -13,19 +13,27 @@ catch {
 Describe -Name 'Infrastructure Tests' -Fixture {
 
     Context -Name 'PSGES3Buckets.yml' -Fixture {
+
         It -Name 'Created the S3 buckets needed for the project' -Test {
-            $assertion1 = ($cfnExports | Where-Object { $_.Name -eq "StageTriggerBN" }).Value
+            $assertion1 = ($cfnExports | Where-Object { $_.Name -eq 'StageTriggerBN' }).Value
             $expected = 'psge-*'
             $assertion1 | Should -BeLike $expected
-            $assertion2 = ($cfnExports | Where-Object { $_.Name -eq "GitXMLDataBN" }).Value
+            $assertion2 = ($cfnExports | Where-Object { $_.Name -eq 'GitXMLDataBN' }).Value
             $expected = 'psge-*'
             $assertion2 | Should -BeLike $expected
-            $assertion4 = ($cfnExports | Where-Object { $_.Name -eq "PubXMLDataBN" }).Value
+            $assertion4 = ($cfnExports | Where-Object { $_.Name -eq 'PubXMLDataBN' }).Value
             $expected = 'psge-*'
             $assertion4 | Should -BeLike $expected
-            $logEval = ($cfnExports | Where-Object { $_.Name -eq "S3BucketLogsBN" }).Value
+            $logEval = ($cfnExports | Where-Object { $_.Name -eq 'S3BucketLogsBN' }).Value
             $logEval | Should -Not -BeNullOrEmpty
         }#it
-    }#context_IntegrationTestInfrastructure
+
+        It -Name 'Should create a SNS topic for S3 trigger updates' -Test {
+            $assertion = ($cfnExports | Where-Object { $_.Name -eq 'UpdateSNSTopicArn' }).Value
+            $expected = 'arn:aws:sns:{0}:{1}:' -f $env:AWSRegion, $env:AWSAccountId
+            $assertion | Should -BeLike $expected
+        }#it
+
+    }#context_PSGES3Buckets
 
 }#describe_infra_tests
